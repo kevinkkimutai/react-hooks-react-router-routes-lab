401d2d7f85069922646e9122c6d268a6957b87bb
kevinkkimutai <kiruikevinkim@gmail.com>
2023-01-18 17:02:28 +0300

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,35 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-
+import { NavLink} from "react-router-dom";
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      {/* home navlink */}
+      <NavLink
+        to="/"
+        exact
+        activeStyle={{ background: "darkblue"}}>
+        Home
+      </NavLink>
+          {/* movies navlink */}
+        <NavLink
+        to="/movies"
+        activeStyle={{ background: "darkblue",}}>
+          Movies
+        </NavLink>
+        {/* directors navlink */}
+      <NavLink
+        to="/directors"
+        activeStyle={{background: "darkblue",}}>
+            Directors
+          </NavLink>
+        {/* actors navlink */}
+        <NavLink
+        to="/actors"
+        activeStyle={{ background: "darkblue",}}>
+          Actors
+          </NavLink>
+    </div>
+  )
 }
 
 export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Routes } from "react-router-dom";
 import App from "./components/App";
 
 ReactDOM.render(
-  <Router>
+
+  <Routes>
     <App />
-  </Router>,
+  </Routes>,
+  
   document.getElementById("root")
 );
